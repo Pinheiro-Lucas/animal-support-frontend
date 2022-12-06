25adaf4df1a9c54e096cfc5a5d188d78494261b0
Pinheiro-Lucas <lucas.gpinheiro2002@gmail.com>
2022-12-06 19:38:35 -0300

    :bug: Landingpage button names

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,14 @@ import CategoryButton from "../components/CategoryButton";
 function Home() {
     return (
         <div className="w-screen h-screen p-5 content-center text-text font-montserrat bg-zinc-100">
-            <ButtonCard title='Encontre seu novo PET conosco' desc='Clique aqui' url_for='/#'/>
+            <ButtonCard title='Encontre seu novo PET conosco' desc='Clique aqui' url_for='/feed'/>
                 <div className="grid grid-cols-2 gap-4 place-content-center">
-                    <CategoryButton title='Adoção'/>
-                    <CategoryButton title='Adoção'/>
-                    <CategoryButton title='Adoção'/>
-                    <CategoryButton title='Adoção'/>
-                    <CategoryButton title='Adoção'/>
-                    <CategoryButton title='Adoção'/>
+                    <CategoryButton title='Doar'/>
+                    <CategoryButton title='Adotar'/>
+                    <CategoryButton title='Vender'/>
+                    <CategoryButton title='Comprar'/>
+                    <CategoryButton title='Achado'/>
+                    <CategoryButton title='Perdido'/>
                 </div>
         </div>
     );
