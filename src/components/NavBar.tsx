8c81768d19211629192798992b7358f9ec0ed8a5
Pinheiro-Lucas <lucas.gpinheiro2002@gmail.com>
2022-12-06 20:09:56 -0300

    :bug: Navbar sticky fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
-        <div className="absolute inset-x-0 bottom-3 h-16 bg-white drop-shadow-navbar m-5 rounded-3xl flex items-center text-2xl text-primary">
+        <div className="absolute sticky inset-x-0 bottom-3 h-16 bg-white drop-shadow-navbar m-5 rounded-3xl flex items-center text-2xl text-primary">
             <Link to='/' className='flex-auto flex'>
                 <AiFillHome className='flex-auto'/>
             </Link>
@@ -16,9 +16,9 @@ function NavBar() {
             <Link to='' className='flex-auto flex'>
                 <AiFillAppstore className='flex-auto'/>
             </Link>
-            <Link to='https://github.com/Pinheiro-Lucas/animal-support-project' className='flex-auto flex'>
+            <a href='https://github.com/Pinheiro-Lucas/animal-support-project' target='_blank' className='flex-auto flex'>
                 <AiFillGithub className='flex-auto'/>
-            </Link>
+            </a>
         </div>
     );
 }
